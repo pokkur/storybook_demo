4203a93ddd21a279c71480bf401d88635e10fa9c
kobayashi yoshiki <kobayashi-yos@gnavi.co.jp>
2020-04-13 13:21:59 +0900

    add ImageFrame

---
+++ src/components/ImageFrame.vue
@@ -0,0 +1,47 @@
+<template>
+  <transition appear>
+    <img :src="formedPath" @load="onLoad" @error="onError" />
+  </transition>
+</template>
+
+<script>
+import PLACEHOLDER_PATH from "@/assets/logo.png";
+
+export default {
+  name: 'ImageFrame',
+  props: {
+    path: {
+      type: String,
+      default: null,
+      require: true
+    }
+  },
+  data() {
+    return {
+      formedPath: this.path
+    }
+  },
+  methods: {
+    onLoad() {
+      // TODO: loaded処理なんらか
+    },
+    onError() {
+      this.$el.src = PLACEHOLDER_PATH // TODO: 代替画像
+      this.$el.classList.add('unloaded')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+img {
+  display: inline-block;
+  border: 0;
+  max-width: 100%;
+  background: #fff url(../assets/91.gif) no-repeat center center; // TODO: ローダーのパス仮
+
+  &.unloaded {
+    background: none;
+  }
+}
+</style>
--- src/pages/One.vue
+++ src/pages/One.vue
@@ -2,6 +2,8 @@
   <div class="one">
     <layer-block>
       <HelloWorld message="How is it Storybook?" />
+      <ImageFrame path="//incloop.com/blog_neta/test/big.png?data=1" width="200" height="200" />
+      <ImageFrame path="//nothing.png" width="150" height="150" />
     </layer-block>
   </div>
 </template>
@@ -9,12 +11,15 @@
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
 import LayerBlock from '@/components/LayerBlock.vue'
+import ImageFrame from '@/components/ImageFrame.vue'
+
 
 export default {
   name: 'One',
   components: {
     HelloWorld,
-    LayerBlock
+    LayerBlock,
+    ImageFrame
   }
 }
 </script>
